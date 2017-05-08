0f1c6119b3b29fd66eb5d8535752ea1782cc5c9b
Alexandre Magno Monteiro <a.mmagno@yahoo.com.br>
2017-05-07 23:17:30 -0300

    dele index.js from root directory

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './Root'
-import 'bootstrap-theme'
-
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
-);
